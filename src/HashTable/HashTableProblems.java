42220f91118387c5c6c1d1994d889764d8aa261b
sleepingchris <chris92wang@gmail.com>
2019-04-06 22:48:17 -0400

    added isHappy problem solution

--- src/HashTable/HashTableProblems.java
+++ src/HashTable/HashTableProblems.java
@@ -1,4 +1,23 @@
 package HashTable;
 
+import java.util.HashSet;
+
 public class HashTableProblems {
+    public static void main(String[] args) {
+        System.out.println(isHappy(20));
+    }
+    public static boolean isHappy(int n) {
+        HashSet<Integer> set = new HashSet<>();
+        while (n != 1) {
+            if (!set.add(n))
+                return false;
+            int result = 0;
+            while (n > 0) {
+                result += (n%10) * (n%10);
+                n /= 10;
+            }
+            n = result;
+        }
+        return true;
+    }
 }
