25450c2a180a47690571d63bad54eb0a93a64d0b
sleepingchris <chris92wang@gmail.com>
2019-04-14 12:06:29 -0400

    added iterative and recursive reverse single linked list

--- src/LinkedList/LinkedListProblems.java
+++ src/LinkedList/LinkedListProblems.java
@@ -18,32 +18,45 @@ public class LinkedListProblems {
         node.next.next = new ListNode(3);
         node.next.next.next = new ListNode(4);
         node.next.next.next.next = new ListNode(5);
-        reverseSingleLinkedList(node);
+        ListNode newNode = reverseSingleLinkedList_Recursive(node);
+        System.out.println(printNode(newNode));
     }
     // reverse single linked list - recursively
     public static ListNode reverseSingleLinkedList_Recursive(ListNode head) {
-        if (head == null)
-            return null;
-       
+        // head is null or last node or only one node
+        if(head == null || head.next == null) {
+            return head;
+        }
+
+        // reverse the rest
+        ListNode newHeadNode = reverseSingleLinkedList_Recursive(head.next);
+
+        // change references for middle chain
+        head.next.next = head;
+        head.next = null;
+
+        // send back new head node in every recursion
+        return newHeadNode;
     }
-    // reverse single linked list - iteratively
-    public static ListNode reverseSingleLinkedList(ListNode head) {
-        if (head == null)
-            return null;
-        List<ListNode> list = new LinkedList<>();
-        ListNode curr = head;
-        list.add(curr);
-        while (curr.next != null) {
-            curr = curr.next;
-            list.add(curr);
+    // iterative solution 2
+    public static ListNode reverseList_Iterative(ListNode head) {
+        //Iterative solution.
+        ListNode last = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = last;
+            last = head;
+            head = next;
         }
-        ListNode newHead = list.get(list.size() - 1);
-        curr = newHead;
-        for (int i=list.size()-2;i>=0;i--) {
-            curr.next = list.get(i);
-            curr = curr.next;
+        return last;
+
+    }
+    static String printNode(ListNode head) {
+        String ret = "" + head.val;
+        while (head.next != null) {
+            head = head.next;
+            ret += " -> " + head.val;
         }
-        curr.next = null;
-        return newHead;
+        return ret;
     }
 }
