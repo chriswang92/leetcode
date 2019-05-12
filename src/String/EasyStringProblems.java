959d2930c097a4e5376f80d17e86ca52478fb891
sleepingchris <chris92wang@gmail.com>
2019-05-12 09:31:27 -0400

    Create EasyStringProblems.java

---
+++ src/String/EasyStringProblems.java
@@ -0,0 +1,11 @@
+package String;
+
+public class EasyStringProblems {
+    public static void main(String[] args) {
+
+    }
+    //38. Count and Say
+    public static String countAndSay(int n) {
+
+    }
+}
