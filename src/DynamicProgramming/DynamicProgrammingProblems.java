362a433ce5ce2ab6fc1552b2a738b25854087aab
Chenguang Wang <wcg19920705@gmail.com>
2019-05-12 22:05:19 -0400

    Update DynamicProgrammingProblems.java

--- src/DynamicProgramming/DynamicProgrammingProblems.java
+++ src/DynamicProgramming/DynamicProgrammingProblems.java
@@ -2,10 +2,11 @@ package DynamicProgramming;
 
 public class DynamicProgrammingProblems {
     public static void main(String[] args) {
-
+        System.out.println(climbStairs(4));
     }
     //70. Climbing Stairs
     public static int climbStairs(int n) {
-
+        if (n == 1) return 1;
+        return
     }
 }
