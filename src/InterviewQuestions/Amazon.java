550988ea2736822f46480d3ccaf5705c77f4e749
chris wang <wcg19920705@gmail.com>
2020-05-31 13:22:16 -0400

    Added amazon interview question: findDistinctIslands using DFS

--- src/InterviewQuestions/Amazon.java
+++ src/InterviewQuestions/Amazon.java
@@ -80,91 +80,168 @@ public class Amazon {
      */
 
     // get all building positions
-    static Map<Integer, List<Integer>> getAllBuildings(int row, int col, List<List<Integer>> grid) {
-        // key is row, value is list of columns
-        Map<Integer, List<Integer>> buidlings = new HashMap<>();
-        for (int r=0;r<row;r++) {
-            for (int c=0;c<col;c++) {
-                if (grid.get(r).get(c) == 1) {
-                    if (buidlings.get(r) == null) {
-                        buidlings.put(r, new ArrayList<>());
-                    }
-                    buidlings.get(r).add(c);
-                }
-            }
-        }
-        return buidlings;
+//    static Map<Integer, List<Integer>> getAllBuildings(int row, int col, List<List<Integer>> grid) {
+//        // key is row, value is list of columns
+//        Map<Integer, List<Integer>> buidlings = new HashMap<>();
+//        for (int r=0;r<row;r++) {
+//            for (int c=0;c<col;c++) {
+//                if (grid.get(r).get(c) == 1) {
+//                    if (buidlings.get(r) == null) {
+//                        buidlings.put(r, new ArrayList<>());
+//                    }
+//                    buidlings.get(r).add(c);
+//                }
+//            }
+//        }
+//        return buidlings;
+//    }
+//
+//    // get all adjacent buildings for each building position
+//    static Map<Integer[], List<Integer[]>> getBuildingsWithAdjacents(int row, int col, List<List<Integer>> grid) {
+//        // key is row, value is list of columns
+//        Map<Integer[], List<Integer[]>> map = new HashMap<>();
+//        for (int r=0;r<row;r++) {
+//            for (int c=0;c<col;c++) {
+//                if (grid.get(r).get(c) == 1) {
+//                    Integer[] position = new Integer[]{r,c};
+//                    if (map.get(position) == null) {
+//                        map.put(position, new ArrayList<>());
+//                    }
+//                    Integer[] pos = null;
+//                    // Add adjacents of the position
+//                    if (c-1>0 && grid.get(r).get(c-1) == 1) {
+//                        pos = new Integer[]{r,c-1};
+//                        map.get(position).add(pos);
+//                    }
+//                    if (c+1<col && grid.get(r).get(c+1) == 1) {
+//                        pos = new Integer[]{r,c+1};
+//                        map.get(position).add(pos);
+//                    }
+//                    if (r-1>0 && grid.get(r-1).get(c) == 1) {
+//                        pos = new Integer[]{r-1,c};
+//                        map.get(position).add(pos);
+//                    }
+//                    if (r+1 < row && grid.get(r+1).get(c) == 1) {
+//                        pos = new Integer[]{r+1,c};
+//                        map.get(position).add(pos);
+//                    }
+//                }
+//            }
+//        }
+//        return map;
+//    }
+//    static List<Integer[]> getAdjacentsByPosition(int row, int col, Map<Integer[], List<Integer[]>> buildingsWithAdjacents) {
+////        List<Integer[]> adjacents = new ArrayList<>();
+//        for (Map.Entry<Integer[], List<Integer[]>> entry : buildingsWithAdjacents.entrySet()) {
+//            Integer[] pos = entry.getKey();
+//            if (pos[0] == row && pos[1] == col) {
+//                return entry.getValue();
+//            }
+//        }
+//        return null;
+//    }
+//
+//    static int visit(Integer[] pos, int rows, int columns, Map<Integer[], List<Integer[]>> buildingsWithAdjacents, List<List<Integer>> visited) {
+//
+//        int row = pos[0], col = pos[1];
+//
+//        if (row < 0 || row > rows - 1 || col < 0 || col > columns - 1) {
+//            return 0;
+//        }
+//        int res = 0;
+//        if (!visited.get(row).contains(col)) {
+//            visited.get(row).add(col);
+//            res++;
+//        }
+//        List<Integer[]> adjacents = getAdjacentsByPosition(row, col, buildingsWithAdjacents);
+//
+//        for (Integer[] adjacentPos : adjacents) {
+//            res += visit(adjacentPos, rows, columns, buildingsWithAdjacents, visited);
+//        }
+//        return res;
+//    }
+    static boolean isSafe(List<List<Integer>> grid, int row, int col, int rows, int columns) {
+        /**
+         * row and col are both in range, and the position is a building(1) and not visited
+         */
+        return (row >= 0) && (row < rows) && col >= 0 && col < columns
+                && grid.get(row).get(col) == 1; //&& !visited[row][col];
     }
+    static void DFS(List<List<Integer>> grid, int row, int col, int rows, int columns
+//            ,boolean[][] visited
+    ) {
+        System.out.printf("\nDFS on (%d, %d)",row,col);
+//        visited[row][col] = true; // use another structure to save visited positions
+        grid.get(row).set(col,-1); // or mark visited by this, but will modify original grid
+        /*
+        Each DFS direction based on follow directions
+         */
+        int[] rowNbr1 = new int[]{-1,0,1,0}; // up, right, down, left
+        int [] colNbr1 = new int[]{0,1,0,-1};
 
-    // get all adjacent buildings for each building position
-    static Map<Integer[], List<Integer[]>> getBuildingsWithAdjacents(int row, int col, List<List<Integer>> grid) {
-        // key is row, value is list of columns
-        Map<Integer[], List<Integer[]>> map = new HashMap<>();
-        for (int r=0;r<row;r++) {
-            for (int c=0;c<col;c++) {
-                if (grid.get(r).get(c) == 1) {
-                    Integer[] position = new Integer[]{r,c};
-                    if (map.get(position) == null) {
-                        map.put(position, new ArrayList<>());
-                    }
-                    Integer[] pos = null;
-                    // Add adjacents of the position
-                    if (c-1>0 && grid.get(r).get(c-1) == 1) {
-                        pos = new Integer[]{r,c-1};
-                        map.get(position).add(pos);
-                    }
-                    if (c+1<col && grid.get(r).get(c+1) == 1) {
-                        pos = new Integer[]{r,c+1};
-                        map.get(position).add(pos);
-                    }
-                    if (r-1>0 && grid.get(r-1).get(c) == 1) {
-                        pos = new Integer[]{r-1,c};
-                        map.get(position).add(pos);
-                    }
-                    if (r+1 < row && grid.get(r+1).get(c) == 1) {
-                        pos = new Integer[]{r+1,c};
-                        map.get(position).add(pos);
-                    }
-                }
+        int[] rowNbr2 = new int[]{-1,0,0,1}; // up, right, left, down
+        int [] colNbr2 = new int[]{0,1,-1,0};
+
+        int[] rowNbr = new int[]{0,0,-1,1}; // left, right, up ,down
+        int [] colNbr = new int[]{-1,1,0,0};
+
+        for (int k=0;k<4;++k) {
+            if (isSafe(grid, row+ rowNbr[k], col + colNbr[k], rows, columns)) {
+                DFS(grid, row + rowNbr[k], col + colNbr[k], rows, columns);
             }
         }
-        return map;
     }
-    static int scan(Map<Integer, List<Integer>> buildings, Map<Integer[], List<Integer[]>> buildingsWithAdjacents) {
-        List<Integer[]> scanned = new ArrayList<>();
-        for (Integer r: buildings.keySet()) {
-            for (Integer c: buildings.get(r)) {
-                Integer[] position = new Integer[]{r,c};
-                if (scanned.contains(position)) {
-                    continue;
-                }
-                List<Integer[]> adjacents = buildingsWithAdjacents.get(position);
-                if (adjacents != null) {
-                    for (Integer[] adjacent: adjacents) {
-                        scanned.add(adjacent);
-                    }
-                }
-                scanned.add(position);
-            }
-        }
-        return 0;
-    }
-    // METHOD SIGNATURE BEGINS, THIS METHOD IS REQUIRED
+    /*
+    https://www.geeksforgeeks.org/find-number-of-islands/
+     */
     static int numberAmazonGoStores(int rows, int columns, List<List<Integer>> grid)
     {
-        int n = 0;
-        // 1. Put all buildings in a List<Integer[]> each value is [row,column]
-        Map<Integer, List<Integer>> buildings = getAllBuildings(rows,columns,grid);
+        int count = 0;
+//        boolean[][] visited = new boolean[rows][columns];
 
-        // 2. Put all buidlings in a map<Integer[], List<Integer[]>>, key is position of the building, value is positions of all adjacent buildings
-        Map<Integer[], List<Integer[]>> buildingsWithAdjacents = getBuildingsWithAdjacents(rows,columns,grid);
+        for (int i=0;i<rows;++i) {
+            for (int j=0;j<columns;++j) {
+                if (grid.get(i).get(j) == 1
+//                && !visited[i][j]
+                ) {
+                    /*
+                    If a cell with value 1 is not visited yet, then new island found,
+                     Visit all cells in this island and increment island count
+                     */
+//                    DFS(grid, i, j, visited, rows, columns);
+                    DFS(grid, i, j, rows, columns);
+                    ++count; // count = how many DFS initialized
+                }
+            }
+        }
 
-        /* 3. Traverse through buildingsWithAdjacents.
-        if the position is not scanned before, process; otherwise skip to next pos
-        Add itself and all adjacents to a list of scanned positions
-         */
-        n = scan(buildings, buildingsWithAdjacents);
-        return n;
+        return count;
+    }
+    /*
+    Amazon phone interview question Advanced: Get distinct islands with different area
+    https://leetcode.com/discuss/interview-question/316156/Amazon-phone-screen-Number-of-distinct-islands/295316
+     */
+    static int getDistinctCount(int[][] grid) {
+        int size = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int r=0;r<grid.length;r++) {
+            for (int c=0;c<grid[0].length;c++) {
+                if (grid[r][c] == 1) {
+                    set.add(getArea(r,c,grid)); // set size = how many DFS initialized
+                }
+            }
+        }
+        return set.size();
+    }
+    /*
+    Use DFS to getArea
+     */
+    static int getArea(int r, int c, int[][] grid) {
+        if (r<0 || r>grid.length-1 || c<0 || c>grid[0].length-1 || grid[r][c] != 1) {
+            return 0;
+        }
+        grid[r][c] = -1;
+        return 1 + getArea(r+1, c, grid) + getArea(r-1, c, grid) + getArea(r, c-1, grid) + getArea(r, c+1, grid);
     }
     public static void main(String[] args) {
         List<List<Integer>> grid = new ArrayList<>();
@@ -173,7 +250,14 @@ public class Amazon {
         grid.add(Arrays.asList(new Integer[]{0,0,0,0}));
         grid.add(Arrays.asList(new Integer[]{1,0,1,1}));
         grid.add(Arrays.asList(new Integer[]{1,1,1,1}));
-        System.out.println(numberAmazonGoStores(5,4, grid));
+//        System.out.printf("\n%d", numberAmazonGoStores(5,4, grid));
+        int[][] grid2 = new int[5][4];
+        grid2[0] = new int[]{1,1,0,0};
+        grid2[1] = new int[]{0,0,1,0};
+        grid2[2] = new int[]{0,0,0,0};
+        grid2[3] = new int[]{1,0,1,1};
+        grid2[4] = new int[]{1,1,1,1};
+        System.out.printf("\n getDistinctCount: %d", getDistinctCount(grid2));
 
 //        System.out.println(generalizedGCD(5, new int[]{2,4,6,8,10}));
 //        System.out.println(cellCompete(new int[]{1,0,0,0,0,1,0,0},1));
